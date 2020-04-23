0d5ee138995219b7ef94bce742384c2119940929
shiyuquan <550765252@qq.com>
2020-04-23 13:50:10 +0800

    修改server ip

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -3,7 +3,8 @@
  */
 export class Constant {
   public serverPort = "9000";
-  public serverIp = "http://localhost";
+  // public serverIp = "http://localhost";
+  public serverIp = "http://192.168.103.33";
   public api = "/api";
   public apiV1 = "/v1";
 }
